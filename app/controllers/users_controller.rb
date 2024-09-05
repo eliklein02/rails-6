60e2707789963f73efbbf17d06411bd5d3448d15
Eli Klein <eliklein02@gmail.com>
2024-09-05 00:02:21 -0400

    added new and create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,16 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def new
+
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:name, :email))
+    @user.save
+    redirect_to @user
+  end
+
+  
 end
