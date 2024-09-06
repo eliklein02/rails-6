5ca0c852009985cca359b5333ebe6ec1de726b01
Eli Klein <eliklein02@gmail.com>
2024-09-05 20:52:09 -0400

    Today's commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,47 @@ class UsersController < ApplicationController
    @id = params[:id]
    @user = User.find(@id)
   end
+
   def index
     @users = User.all
   end
 
   def new
-
+    @user = User.new
   end
 
   def create
     @user = User.new(params.require(:user).permit(:name, :email))
-    @user.save
-    redirect_to @user
+    if @user.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(params.require(:user).permit(:name, :email))
+      flash[:notice] = "User was successfully updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      flash[:notice] = "User deleted"
+      redirect_to users_path
+    else
+      render @user
+    end
+  end
   
 end
