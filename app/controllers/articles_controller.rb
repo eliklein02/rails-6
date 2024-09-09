aa1c6d78178f226acfb896bac7664628ed689580
Eli Klein <eliklein02@gmail.com>
2024-09-08 20:31:36 -0400

    added bcrypt functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,6 @@ class ArticlesController < ApplicationController
   # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
-
     @article.user = User.first
 
     respond_to do |format|
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_08_180129) do
+ActiveRecord::Schema.define(version: 2024_09_09_002151) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2024_09_08_180129) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest"
   end
 
 end
